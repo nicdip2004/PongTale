dd83925b26e96c681292dac7de7ef04246c9c68e
nicdip2004 <129190555+nicdip2004@users.noreply.github.com>
2024-07-09 22:07:41 +0700

    add

---
+++ Assets/script/SceneManagement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+ 
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitApp()
+    {
+        Application.Quit();
+    }
+}
